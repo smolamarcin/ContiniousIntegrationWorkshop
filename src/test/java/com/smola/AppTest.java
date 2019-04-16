8432057355792a1bdb4acd73a45bf881eedb9981
Marcin_Smola <marcin.smola7@gmail.com>
2019-04-16 10:51:33 +0200

    project skeleton

---
+++ src/test/java/com/smola/AppTest.java
@@ -0,0 +1,14 @@
+package com.smola;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
+}
