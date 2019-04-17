7c56838839c25e9d2cb9c6bdbcdb4926419234a9
Marcin_Smola <marcin.smola7@gmail.com>
2019-04-17 11:40:20 +0200

    added surefire maven plugin

---
+++ src/main/java/com/smola/Siema.java
@@ -0,0 +1,4 @@
+package com.smola;
+
+class Siema {
+}
---
+++ src/test/java/com/smola/SiemaTest.java
@@ -0,0 +1,13 @@
+package com.smola;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SiemaTest {
+
+    @Test
+    public void assertsad() {
+        assertTrue(true);
+    }
+}
