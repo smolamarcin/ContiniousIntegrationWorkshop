c96d1bfcb813777f7da2a83c558a6a7f3c12ebd6
Marcin_Smola <marcin.smola7@gmail.com>
2019-04-17 14:36:09 +0200

    Jenkisn file

---
+++ src/test/java/com/smola/AppTestIT.java
@@ -0,0 +1,14 @@
+package com.smola;
+
+import org.junit.jupiter.api.Test;
+
+class AppTestIT {
+    @Test
+    public void sampleIntegrationTest() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
